ffa88deebd55de6fafa5e932cc00504e873e31d4
Mhmdrz <mr.ala72@gmail.com>
2020-05-06 19:46:09 +0430

    root component structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//@ts-nocheck
+import React from "react";
+import "./App.scss";
+import cardsData from "./data";
+import Card from "./components/card";
 
 function App() {
+  const [cards, setCrds] = React.useState([]);
+  const [isLoading, setisLoading] = React.useState(true);
+  const [filteredOptions, setFilteredOptions] = React.useState([]);
+  function filterOptions(filter: string) {
+    setFilteredOptions(cards.filter((card) => card[filter]));
+  }
+  React.useEffect(() => {
+    cardsData.real.then((data) => setCrds(data));
+  }, []);
+  let cardsToRender = filteredOptions.length > 0 ? filteredOptions : cards;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>تمام خانه ها</h1>
+      <div>
+        <button onClick={() => filterOptions("has_picture")}>
+          فقط عکس دار
+        </button>
+      </div>
+      <div className="card-container">
+        {console.log(filteredOptions)}
+        {cardsToRender.map((s) => (
+          <Card
+            date={s.create_timestamp}
+            isVeryfied={s.badge}
+            priceBase={s.mortgage_amount}
+            numberOfRooms={s.room_qty}
+            key={s.catalog_uuid}
+            priceMonthly={s.monthly_rent}
+            approxLocation={s.district_name}
+            metrage={s.unit_area_extent}
+            imgUrl={s.thumbnail_img}
+          />
+        ))}
+      </div>
+    </main>
   );
 }
 
